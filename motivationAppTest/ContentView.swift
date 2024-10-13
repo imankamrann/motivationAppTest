3d06e9acce19bc75e3290e767d295c961b863392
imankamrann <imankamran@gmail.com>
2024-10-13 19:35:46 -0400

    Initial Commit

---
+++ motivationAppTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  motivationAppTest
+//
+//  Created by Iman on 2024-10-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ motivationAppTest/motivationAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  motivationAppTestApp.swift
+//  motivationAppTest
+//
+//  Created by Iman on 2024-10-13.
+//
+
+import SwiftUI
+
+@main
+struct motivationAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
